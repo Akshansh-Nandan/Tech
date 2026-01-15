ce0da139ea3075a43e39ff0b08153f418c5b36d7
Akshansh-Nandan <125875752+Akshansh-Nandan@users.noreply.github.com>
2026-01-15 21:47:09 +0530

    Delete AI_lib directory
    New version of AIDK

--- AI_lib/aidk/ai_dev_kit.py
+++
@@ -1,6 +0,0 @@
-import aidk.tranformers_module as transformers_kit
-import aidk.neuralnet_module as neuralnet_kit
-import aidk.prediction_module as prediction_kit
-import aidk.neuromorphic_nn as neuromorphic_nn
-
-
